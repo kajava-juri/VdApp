43123f3b94f19b310de1adca0bc6c85b80023771
Jüri Kajava <jyri.kajava@tptlive.ee>
2022-11-26 15:33:05 +0200

    increased the clickable are to open video on fullscreen

--- Components/Media.js
+++ Components/Media.js
@@ -7,8 +7,11 @@ export default function Media({file, checkboxClick, showDelete, isLoggedIn, onMe
             {isLoggedIn && (
               <input type="checkbox" className="regular-checkbox big-checkbox" data-filename={file.Path} onClick={checkboxClick} id={file.Id}/>
             )}
-            <img height="210" width="100%" className="myVid" src={`videos/${file.Path}`}></img>
-            <p>{file.Path}</p>
+            <div className="myVid">
+              <img height="210" width="100%" src={`videos/${file.Path}`}></img>
+              <p>{file.Path}</p>
+            </div>
+
             {(showDelete && isLoggedIn) && (
               <button onClick={() => Delete(file.Path)}>DELETE</button>
             )}
@@ -23,9 +26,12 @@ export default function Media({file, checkboxClick, showDelete, isLoggedIn, onMe
               <input type="checkbox" className="regular-checkbox big-checkbox" data-filename={file.Path} onClick={checkboxClick} id={file.Id}/>
             )}
 
-            <video src={`videos/${file.Path}`} height="210" width="100%" className="myVid" onClick={() => onMediaClick(file.Path)}>
-            </video>
-            <p>{file.Path}</p>
+            <div onClick={() => onMediaClick(file.Path)} className="myVid">
+              <video src={`videos/${file.Path}`} height="210" width="100%"  >
+              </video>
+              <p>{file.Path}</p>
+            </div>
+
             {(showDelete && isLoggedIn) && (
               <button onClick={() => Delete(file.Path)}>DELETE</button>
             )}
