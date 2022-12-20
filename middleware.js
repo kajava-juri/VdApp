767b0369b26df064d6528d0e48bf158563ae8cff
Jüri Kajava <jyri.kajava@tptlive.ee>
2022-12-20 21:00:10 +0200

    middleware

---
+++ middleware.js
@@ -0,0 +1,33 @@
+import { NextResponse } from "next/server";
+import { getIronSession } from "iron-session/edge";
+import { sessionOptions } from "./lib/session";
+
+export async function middleware(req) {
+    const res = NextResponse.next();
+    const session = await getIronSession(req, res, sessionOptions);
+    const { user } = session;
+
+    if (req.nextUrl.pathname.startsWith('/api/fileUploads')) {
+        if(user?.isLoggedIn){
+            console.log("uploading files while authorized");
+        } else {
+            return new NextResponse(
+                JSON.stringify({ success: false, message: 'authentication failed' }),
+                { status: 401, headers: { 'content-type': 'application/json' } }
+              )
+        }
+    }
+
+    if (req.nextUrl.pathname.startsWith('/api/fileSearch')) {
+        if(user?.isLoggedIn){
+            console.log("searching files while authorized");
+        } else {
+            return new NextResponse(
+                JSON.stringify({ success: false, message: 'authentication failed' }),
+                { status: 401, headers: { 'content-type': 'application/json' } }
+              )
+        }
+    }
+
+    return res;
+}
